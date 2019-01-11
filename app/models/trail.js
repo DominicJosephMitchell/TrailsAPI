a2101f8ac5fb96c67eb89fdd4740028940de74cc
nstco29 <nstco29@gmail.com>
2019-01-11 11:08:54 -0500

    create route scripts models

---
+++ app/models/trail.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const trailSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    subtitle: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},
+    {
+        timestamps: true
+    })
+
+module.exports = mongoose.model('Trail', trailSchema)
---
+++ app/routes/trails_routes.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const passport = require('passport')
+
+const Trail = require('../models/trail')
+
+const handle = require('../../lib/error_handler')
+const customErrors = require('../../lib/custom_errors')
+
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+
+const requireToken = passport.authenticate('bearer', { session: false })
+
+const router = express.Router()
+
+router.get('/trails', requireToken, (req, res) => {
+  Trail.find({ owner: req.user.id })
+    .then(trails => {
+        return trails.map(trail => trail.toObject())
+    })
+      .then(trails => res.status(200).json({ trails: trails }))
+    .catch(err => handle(err, res))
+})
+
+router.get('/trails/:id', requireToken, (req, res) => {
+    Trail.findById(req.params.id)
+    .then(handle404)
+        .then(trail => res.status(200).json({ trail: trail.toObject() }))
+    .catch(err => handle(err, res))
+})
+
+router.post('/trails', requireToken, (req, res) => {
+    req.body.trail.owner = req.user.id
+
+    Trail.create(req.body.trail)
+        .then(trail => {
+            res.status(201).json({ trail: trail.toObject() })
+    })
+    .catch(err => handle(err, res))
+})
+
+router.patch('/trails/:id', requireToken, (req, res) => {
+    delete req.body.trail.owner
+
+    Trail.findById(req.params.id)
+    .then(handle404)
+        .then(trail => {
+            requireOwnership(req, trail)
+
+            Object.keys(req.body.trail).forEach(key => {
+                if (req.body.trail[key] === '') {
+                    delete req.body.trail[key]
+        }
+      })
+
+            return trail.update(req.body.trail)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(err => handle(err, res))
+})
+
+router.delete('/trails/:id', requireToken, (req, res) => {
+    Trail.findById(req.params.id)
+    .then(handle404)
+        .then(trail => {
+            requireOwnership(req, trail)
+            trail.remove()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(err => handle(err, res))
+})
+
+module.exports = router
