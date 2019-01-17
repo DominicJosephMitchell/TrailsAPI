e4bc3396822a58c4e667cfbf49e2e8917be27644
nstco29 <nstco29@gmail.com>
2019-01-17 15:52:58 -0500

    update db.jsy

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'trails-api'
+const mongooseBaseName = 'obscure-brook-97374'
 
 // create the mongodb uri for development and test
 const database = {
