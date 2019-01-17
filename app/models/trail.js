1d715564e962136fd9d131c69434e1ea83071c38
nstco29 <nstco29@gmail.com>
2019-01-17 14:50:05 -0500

    update app

--- app/models/trail.js
+++ app/models/trail.js
@@ -24,36 +24,3 @@ const trailSchema = new mongoose.Schema({
     })
 
 module.exports = mongoose.model('Trail', trailSchema)
-
-// const mongoose = require('mongoose')
-
-// const trailSchema = new mongoose.Schema({
-//     trail: {
-//         type: null,
-//         required: true
-//     },
-//     completed: {
-//         type: boolean,
-//         required: true
-//     },
-//     completed_trail: {
-//         type: null,
-//         required: true
-//     },
-//     completedTrailId: props.id {
-//         type: boolean,
-//         required: true
-//     },
-
-//     owner: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//         required: true
-//     },
-
-//     {
-//         timestamps: true
-//     })
-
-// module.exports = mongoose.model('Trail', trailSchema)
-
